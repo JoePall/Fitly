d0c4b0a2747ac6d159d4fda9448512abef34d68a
Josiah Powell <josiahpowell@outlook.com>
2020-10-10 17:48:26 -0500

    Converting to handlebars

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const express = require("express");
+const fs = require("fs");
 const mongoose = require("mongoose");
-
+const handlebars = require("express-handlebars");
 const PORT = process.env.PORT || 3000
-
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget",
-     {
-       useNewUrlParser: true,
-       useUnifiedTopology: true,
-       useCreateIndex: true,
-       useFindAndModify: false
-     }
-   );
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
-// app.use(require("./public/api.js"));
+// get all controllers
+fs.readdirSync("./controllers")
+  .filter(file => file.indexOf(".") !== 0 && file.slice(-3) === ".js")
+  .forEach(file => require("./controllers/" + file)(app));
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
